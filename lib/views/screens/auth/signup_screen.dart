8bdff25185995972ae036c18fc13cb98eb0936e8
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-20 14:13:05 +0700

    created Register Screen

---
+++ lib/views/screens/auth/signup_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_app/constants.dart';
+import '../../widgets/text_input_field.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({Key? key}) : super(key: key);
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "TikTok App",
+              style: TextStyle(
+                  fontSize: 35,
+                  color: buttonColor,
+                  fontWeight: FontWeight.w900),
+            ),
+            const Text(
+              "Register",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _usernameController,
+                labelText: 'Username',
+                icon: Icons.person,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _emailController,
+                labelText: 'Email',
+                icon: Icons.email,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextInputField(
+                controller: _passwordController,
+                labelText: 'Password',
+                icon: Icons.lock,
+                isObscure: true,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: buttonColor,
+                  borderRadius: const BorderRadius.all(Radius.circular(5))),
+              child: InkWell(
+                onTap: () => {print("Register")},
+                child: const Center(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Already have an account? ",
+                  style: TextStyle(fontSize: 20),
+                ),
+                InkWell(
+                  onTap: () {
+                    print("navigation in login");
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20, color: buttonColor),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
