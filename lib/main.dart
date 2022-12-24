394d54563bae1a72d06d080a81021197a6bdd9b6
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-24 07:42:53 +0700

    restotasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_app/constants.dart';
 import 'package:tiktok_app/views/screens/auth/login_screen.dart';
 import 'package:tiktok_app/views/screens/auth/signup_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
