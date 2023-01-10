479765a906aef5af21674a6c7613afc4cdce8e9f
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-10 19:48:53 +0700

    cretaed user register

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,58 @@
+import 'package:get/get.dart';
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:tiktok_application/constants.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktok_application/models/user.dart' as model;
+
+class AuthController extends GetxController {
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  void registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        // save out user to our ath and firebase firestore
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+        model.User user = model.User(
+          name: username,
+          email: email,
+          uid: cred.user!.uid,
+          profilePhoto: downloadUrl,
+        );
+        await firestore
+            .collection('users')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
+      } else {
+        Get.snackbar(
+          'Error Creating Account',
+          'Please enter all the fields',
+        );
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
+  }
+}
